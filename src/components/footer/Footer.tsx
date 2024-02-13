d622a97aa1fd2047156203917cbb8e0d4d818c82
Vitali Zhybuliou <zhybuliou.v.v@gmail.com>
2024-02-13 19:15:36 +0300

    feat: add footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,3 +1,13 @@
+import './footer.css';
+
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <div className="footer-wrapper">
+        <div className="footer-content">
+          © 2020 – <b>Learning Bridge.</b> All rights reserved.
+        </div>
+      </div>
+    </footer>
+  );
 }
--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,11 +1,13 @@
 import Footer from '../../components/footer/Footer';
 import Header from '../../components/header/Header';
+import './home-page.css';
 
 export default function HomePage() {
   return (
-    <>
+    <div className="home-wrapper">
       <Header />
+      <main>main</main>
       <Footer />
-    </>
+    </div>
   );
 }
