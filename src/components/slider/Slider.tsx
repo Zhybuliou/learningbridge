3599a8140c640b2739b5d5949485a42a6c440f38
Vitali Zhybuliou <zhybuliou.v.v@gmail.com>
2024-02-14 18:34:50 +0300

    feat: add style slider

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -13,32 +13,34 @@ export default function SimpleSlider() {
     autoplay: true,
     autoplaySpeed: 5000,
     slidesToShow: 1,
-    className: 'main-page-slider',
+    className: 'center',
     adaptiveHeight: true,
     cssEase: 'linear',
   };
   return (
     <div className="slider-container">
-      <Slider {...settings}>
-        <div>
-          <h3>1</h3>
-        </div>
-        <div>
-          <h3>2</h3>
-        </div>
-        <div>
-          <h3>3</h3>
-        </div>
-        <div>
-          <h3>4</h3>
-        </div>
-        <div>
-          <h3>5</h3>
-        </div>
-        <div>
-          <h3>6</h3>
-        </div>
-      </Slider>
+      <div className="main-page-slider">
+        <Slider {...settings}>
+          <div className="main-page-slide">
+            <h3>1</h3>
+          </div>
+          <div className="main-page-slide">
+            <h3>2</h3>
+          </div>
+          <div className="main-page-slide">
+            <h3>3</h3>
+          </div>
+          <div className="main-page-slide">
+            <h3>4</h3>
+          </div>
+          <div className="main-page-slide">
+            <h3>5</h3>
+          </div>
+          <div className="main-page-slide">
+            <h3>6</h3>
+          </div>
+        </Slider>
+      </div>
     </div>
   );
 }
