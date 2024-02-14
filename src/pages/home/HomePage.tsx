be635ff017243b0166d180001c1af049f5b672df
Vitali Zhybuliou <zhybuliou.v.v@gmail.com>
2024-02-14 15:16:31 +0300

    feat: add corusel for home page

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,12 +1,29 @@
 import Footer from '../../components/footer/Footer';
 import Navbar from '../../components/header/nav/Navbar';
+import ImageSlider from '../../components/slider/ImageSlider';
 import './home-page.css';
+
+import car1 from '../../assets/imgs/Jiraiya.png';
+import car2 from '../../assets/imgs/Kakashi.png';
+import car3 from '../../assets/imgs/Madara.png';
+import car4 from '../../assets/imgs/Naruto.png';
+import car5 from '../../assets/imgs/pain.png';
+
+const IMAGES = [
+  { url: car1, alt: 'Car One' },
+  { url: car2, alt: 'Car Two' },
+  { url: car3, alt: 'Car Three' },
+  { url: car4, alt: 'Car Four' },
+  { url: car5, alt: 'Car Five' },
+];
 
 export default function HomePage() {
   return (
     <div className="home-wrapper">
       <Navbar />
-      <main>main</main>
+      <main>
+        <ImageSlider images={IMAGES} />
+      </main>
       <Footer />
     </div>
   );
