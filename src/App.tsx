1705184944ec9e31e3ba61a4e7c9f5c609d4d02b
Vitali Zhybuliou <zhybuliou.v.v@gmail.com>
2024-02-19 20:49:11 +0300

    feat: add pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,11 @@ import StartPage from './pages/start/StartPage';
 import HomePage from './pages/home/HomePage';
 import HomePageLayout from './Layout/HomePageLayout';
 import CurriculumPage from './pages/curriculum/CurriculumPage';
+import AboutPage from './pages/about/AboutPage';
+import CalendarPage from './pages/calendar/CalendarPage';
+import VirtualPage from './pages/virtual/VirtualPage';
+import ContactPage from './pages/contact/ContactPage';
+import Page404 from './pages/404/Page404';
 
 export default function App() {
   return (
@@ -11,11 +16,11 @@ export default function App() {
       <Route path="/freehold" element={<HomePageLayout />}>
         <Route index element={<HomePage />} />
         <Route path="/freehold/curriculum" element={<CurriculumPage />} />
-        <Route path="/freehold/about" element={<CurriculumPage />} />
-        <Route path="/freehold/calendar" element={<CurriculumPage />} />
-        <Route path="/freehold/virtual" element={<CurriculumPage />} />
-        <Route path="/freehold/contact" element={<CurriculumPage />} />
-        <Route path="/freehold/*" element={<CurriculumPage />} />
+        <Route path="/freehold/about" element={<AboutPage />} />
+        <Route path="/freehold/calendar" element={<CalendarPage />} />
+        <Route path="/freehold/virtual" element={<VirtualPage />} />
+        <Route path="/freehold/contact" element={<ContactPage />} />
+        <Route path="/freehold/*" element={<Page404 />} />
       </Route>
     </Routes>
   );
---
+++ src/pages/virtual/VirtualPage.tsx
@@ -0,0 +1,3 @@
+export default function VirtualPage() {
+  return <div>VirtualPage</div>;
+}
