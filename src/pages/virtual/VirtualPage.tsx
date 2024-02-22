9ef2e4bea0011b0ca47703a8692f7b48c46e1087
Vitali Zhybuliou <zhybuliou.v.v@gmail.com>
2024-02-22 16:08:10 +0300

    feat: add content for page Virtual Learning

--- src/pages/virtual/VirtualPage.tsx
+++ src/pages/virtual/VirtualPage.tsx
@@ -1,5 +1,10 @@
 import PageTitle from '../../components/page-title/PageTitle';
 import './virtual.css';
+import BoyBook from '../../assets/kid-who.png';
+import ImageTrophy from '../../assets/trophy.png';
+import ImageBoard from '../../assets/abc.png';
+import ImageBooks from '../../assets/books.png';
+import Imagecertificate from '../../assets/certificate.png';
 
 export default function VirtualPage() {
   return (
@@ -8,6 +13,46 @@ export default function VirtualPage() {
       <div className="virtual-header">
         <p>Learning Bridge Academy</p>
         <h3>Our Virtual Learning</h3>
+      </div>
+      <div className="virtual-content">
+        <div className="virtual-content-image">
+          <img src={BoyBook} alt="boy" width="auto" height="300px" />
+        </div>
+        <div className="virtual-content-info">
+          <div className="who-content-card">
+            <div className="who-content-content">
+              <h5>Dedicated classroom</h5>
+              <p>
+                Learning Bridge Academy has a dedicated classroom for Virtual
+                Learners with a specialist who works with each student.
+              </p>
+            </div>
+          </div>
+          <div className="who-content-card">
+            <div className="who-content-content">
+              <h5>Access Capabilities</h5>
+              <p>
+                Our class has remote access capabilities for parents to monitor
+                their child.
+              </p>
+            </div>
+          </div>
+          <div className="who-content-card">
+            <div className="who-content-content">
+              <h5>Social Distancing</h5>
+              <p>
+                The classroom makes sure to follow the proper safety and social
+                distancing guidelines.
+              </p>
+            </div>
+          </div>
+          <div className="who-content-card">
+            <div className="who-content-content">
+              <h5>Snacks</h5>
+              <p>We also provide meals and snacks on site.</p>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
